98d02dea6acfa487496f2f255bc7f21b16198168
Dihfahsih1 <dihfahsihm@gmail.com>
2024-05-23 21:11:50 +0300

    lists

---
+++ Assignments/practice1.py
@@ -0,0 +1,8 @@
+list1=['bananas','apples','tomatoes',5, 2.5, False]
+# adding value at the end of the list
+list1.append('Mary')
+
+
+#adding a value at a given index or position
+list1.insert(2,'new tomatoes')
+print(list1)
---
+++ program.py
@@ -0,0 +1,7 @@
+
+def multiplication(a,b):
+    x=a*b
+    return x
+print(multiplication(3,4))
+
+
